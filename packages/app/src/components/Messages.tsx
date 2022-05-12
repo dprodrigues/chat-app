c6279eab2ac72be5e4afc67625df476abc06839b
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-05-11 23:07:35 -0300

    refactor(messages): adiciona estilos e nome de usuário

--- packages/app/src/components/Messages.tsx
+++ packages/app/src/components/Messages.tsx
@@ -2,13 +2,18 @@ import { useState, useEffect } from 'react'
 import { useSocketIo } from '../contexts/SocketIo'
 import { NEW_MESSAGE } from '../constants/socketio_events'
 
+interface IMessage {
+  username: string
+  content: string
+}
+
 export default function Messages() {
-  const [messages, setMessages] = useState<string[]>([])
+  const [messages, setMessages] = useState<IMessage[]>([])
   const socket = useSocketIo()
 
   useEffect(() => {
-    socket.on(NEW_MESSAGE, (message: string) => {
-      setMessages((messages) => [...messages, message])
+    socket.on(NEW_MESSAGE, (message: IMessage) => {
+      setMessages((messages) => [message, ...messages])
     })
 
     return () => {
@@ -17,9 +22,23 @@ export default function Messages() {
   }, [socket])
 
   return (
-    <ul>
+    <ul
+      style={{
+        display: 'flex',
+        flexDirection: 'column-reverse',
+        height: '75vh',
+        margin: 0,
+        overflowY: 'scroll',
+        padding: 0,
+        width: '50vw',
+      }}
+    >
       {messages?.map((message, index) => (
-        <li key={index}>{message}</li>
+        <li key={index} style={{ listStyle: 'none' }}>
+          <p style={{ margin: '0 0 8px' }}>
+            <strong>{message.username}</strong>: {message.content}
+          </p>
+        </li>
       ))}
     </ul>
   )
