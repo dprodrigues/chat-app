74d46abb11010f30aeedcb4764c31b21cdf4bf28
Daniel Rodrigues <dandan.dpr@gmail.com>
2022-05-11 23:06:30 -0300

    refactor(home): adiciona title e estilos

--- packages/app/src/pages/Home.tsx
+++ packages/app/src/pages/Home.tsx
@@ -1,12 +1,29 @@
+import ActiveUsers from '../components/ActiveUsers'
 import Messages from '../components/Messages'
 import Form from '../components/Form'
 
 export default function Home() {
   return (
-    <section>
-      <Messages />
+    <>
+      <h1 style={{ textAlign: 'center' }}>Chat App</h1>
 
-      <Form />
-    </section>
+      <section
+        style={{
+          alignItems: 'center',
+          display: 'flex',
+          height: '90vh',
+          justifyContent: 'space-evenly',
+          overflow: 'hidden',
+        }}
+      >
+        <ActiveUsers />
+
+        <article>
+          <Messages />
+
+          <Form />
+        </article>
+      </section>
+    </>
   )
 }
